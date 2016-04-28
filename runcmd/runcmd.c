6b5dfec3b8ead3cb3767068712b41ad14c5b57e6
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2016-04-28 19:13:21 -0300

    Added POSIXeg debug.h. Untested implementation of the first part of the exercise, without exec test.

--- runcmd/runcmd.c
+++ runcmd/runcmd.c
@@ -1,9 +1,64 @@
+#include <debug.h>
 #include <runcmd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAX_ARGS 20
+#define CMD_DELIMITERS " "
 
 int runcmd(const char* command, int* result, const int* io)
 {
-	return 0;
+	char* args[MAX_ARGS], *cmd = NULL, *cur = NULL;
+	int nargs = 0, cstatus;
+	pid_t cpid;
+
+	cmd = (char*) malloc((strlen(command)+1)*sizeof(char));			/* Memory leak on child process */
+	sysfail(cmd == NULL, -1);
+
+	strcpy(cmd, command);
+	strtok(cmd, CMD_DELIMITERS);
+	while (nargs < MAX_ARGS && (cur = strtok(NULL, CMD_DELIMITERS)) != NULL)
+	{
+		args[nargs++] = cur;
+	}
+
+	if (nargs == MAX_ARGS && strtok(NULL, CMD_DELIMITERS) != NULL)
+	{
+		fprintf(stderr, "Error: too many arguments. Runcmd can't handle more than %d arguments.\n", MAX_ARGS);
+		return -1;
+	}
+
+	args[nargs] = NULL;
+
+	cpid = fork();
+	sysfail(cpid < 0, -1);
+
+	if (cpid == 0)
+	{
+		execvp(args[0], args);
+		free(cmd);						/* This and the next line only execute if execvp fails */
+		exit(EXECFAILSTATUS);	
+	}
+	else
+	{
+		waitpid(cpid, &cstatus, 0);
+		if (result != NULL)
+		{
+			*result = 0;
+			if (WIFEXITED(cstatus))
+			{
+				*result |= WEXITSTATUS(cstatus);
+				*result |= NORMTERM;
+			}
+		}
+	}
+	
+	free(cmd);
+	return cpid;
 }
 
 void (*runcmd_onexit)(void) = NULL;
--- runcmd/runcmd.h
+++ runcmd/runcmd.h
@@ -1,6 +1,16 @@
 #ifndef RUNCMD_H
 #define RUNCMD_H
 
+#define EXECFAILSTATUS 29
+#define EXITSTATUSBYTE 0xFF
+
+#define NORMTERM (1 << 8)
+#define EXECOK (1 << 9)
+
+#define EXITSTATUS(ret) ((ret & EXITSTATUSBYTE))
+#define IS_NORMTERM(ret) ((ret & NORMTERM) && 1)
+#define IS_EXECOK(ret) ((ret & EXECOK) && 1)
+
 int runcmd(const char *command, int *result, const int* io);
 extern void (*runcmd_onexit)(void);
 
